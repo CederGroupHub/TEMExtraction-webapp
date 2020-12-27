5b8359ca302bd1545a7d00efe10efee87e875641
Akshay Subramanian <akshaysubramanian99@gmail.com>
2020-12-27 20:02:33 +0530

    Add color key and change visualization code to color particles based on key

--- client/src/Table.js
+++ client/src/Table.js
@@ -4,13 +4,21 @@ import Tab from 'react-bootstrap/Tab'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Display from './DisplayImage';
+import ColorKey from './Shared/img/color_key.png';
 
 export default class Table extends Component {
     render() {
         return (
             <Tabs defaultActiveKey="segmentation" id="uncontrolled-tab-example" style={{fontSize: [2, 3, 4]}}>
                 <Tab tabClassName="color-blue" eventKey="segmentation" title="SEGMENTATION">
-                    <Display image={this.props.segmented_image} />
+                    <div className='row'>
+                        <div className='column'>
+                            <Display image={this.props.segmented_image} />
+                        </div>
+                        <div className='column'>
+                            <img src={ColorKey} alt='' />
+                        </div>
+                    </div>
                 </Tab>
                 <Tab tabClassName='color-blue' eventKey="scale_label" title="SCALE AND LABEL DETECTION">
                     <Display image={this.props.detected_image} />
