9710a7153d210eb7a2638951f672965697765cd2
Akshay Subramanian <akshaysubramanian99@gmail.com>
2021-01-08 20:33:00 +0530

    Modify yaml file pycocotools version

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 export const API_URL = process.env.NODE_ENV === 'production'
   ? 'https://react-image-upload.herokuapp.com'
-  // : 'http://localhost:8080'
   : 'http://localhost:8000'
+  // : 'http://b27bd8606e7e.ngrok.io'
